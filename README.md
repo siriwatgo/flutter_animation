207912eaad0cb30c4c1feb6e83ab1a0a333a8832
siriwatgo <hisiriwat@gmail.com>
2023-03-07 18:38:54 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Animation in Flutter
+
+## Lottie
+Homepage: https://lottiefiles.com/
+
+Dependencies: https://pub.dev/packages/lottie
+
+[![](https://github.com/xvrh/lottie-flutter/workflows/Lottie%20Flutter/badge.svg?branch=master)](https://github.com/xvrh/lottie-flutter)
+[![pub package](https://img.shields.io/pub/v/lottie.svg)](https://pub.dev/packages/lottie)
+
+Lottie is a mobile library for Android and iOS that parses [Adobe After Effects](https://www.adobe.com/products/aftereffects.html) 
+animations exported as json with [Bodymovin](https://github.com/airbnb/lottie-web) and renders them natively on mobile!
+
+This repository is an unofficial conversion of the [Lottie-android](https://github.com/airbnb/lottie-android) library in pure Dart. 
+
+It works on Android, iOS, macOS, linux, windows and web.
+
+## Rive
+Homepage: https://rive.app/
+
+Dependencies: https://pub.dev/packages/rive
+
+Rive Flutter is a runtime library for [Rive](https://rive.app), a real-time interactive design and animation tool.
+
+This library allows you to fully control Rive files with a high-level API for simple interactions and animations, as well as a low-level API for creating custom render loops for multiple artboards, animations, and state machines in a single canvas.
---
+++ lib/main.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:lottie/lottie.dart';
+import 'package:rive/rive.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Animation in Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Animation in Flutter'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  // Lottie
+  late final AnimationController _controllerLottie;
+  late final Future<LottieComposition> _compositionLottie;
+
+  // Rive
+  late RiveAnimationController _controllerRive;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controllerLottie = AnimationController(vsync: this);
+    _compositionLottie = _loadCompositionLottie();
+
+    _controllerRive = SimpleAnimation('idle');
+  }
+
+  // Load Composition Lottie
+  Future<LottieComposition> _loadCompositionLottie() async {
+    var assetData =
+        await rootBundle.load('assets/lottiefiles/LottieLogo1.json');
+    return await LottieComposition.fromByteData(assetData);
+  }
+
+  // Toggles between play and pause animation states
+  void _togglePlay() =>
+      setState(() => _controllerRive.isActive = !_controllerRive.isActive);
+
+  /// Tracks if the animation is playing by whether controller is running
+  bool get isPlaying => _controllerRive.isActive;
+
+  @override
+  void dispose() {
+    _controllerLottie.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              // Load a Lottie file from your assets
+              Semantics(
+                label: 'This is a Lottie',
+                child: LottieBuilder.asset(
+                  "assets/lottiefiles/polar_bear_day.json",
+                  width: 200,
+                  height: 200,
+                  fit: BoxFit.fill,
+                ),
+              ),
+
+              // Load a Lottie file from a remote url
+              Container(
+                height: 200,
+                child: Lottie.network(
+                  'https://raw.githubusercontent.com/xvrh/lottie-flutter/master/example/assets/Mobilo/A.json',
+                  controller: _controllerLottie,
+                  onLoaded: (composition) {
+                    // Configure the AnimationController with the duration of the
+                    // Lottie file and start the animation.
+                    _controllerLottie
+                      ..duration = composition.duration
+                      ..forward();
+                  },
+                ),
+              ),
+
+              // Load an animation and its images from a zip file
+              // Lottie.asset('assets/lottiefiles/angel.zip'),
+
+              // Custom loading Lottie
+              Container(
+                height: 200,
+                child: FutureBuilder<LottieComposition>(
+                  future: _compositionLottie,
+                  builder: (context, snapshot) {
+                    var composition = snapshot.data;
+                    if (composition != null) {
+                      return Lottie(composition: composition);
+                    } else {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                  },
+                ),
+              ),
+
+              // Load a Rive file from a remote url
+              Container(
+                height: 200,
+                child: RiveAnimation.network(
+                  'https://cdn.rive.app/animations/vehicles.riv',
+                  controllers: [_controllerRive],
+                  // Update the play state when the widget's initialized
+                  onInit: (_) => setState(() {}),
+                ),
+              ),
+
+              // Load a Rive file from your assets
+              // RiveAnimation.asset('assets/rivefiles/noon-nap.riv'),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _togglePlay,
+        tooltip: isPlaying ? 'Pause' : 'Play',
+        child: Icon(
+          isPlaying ? Icons.pause : Icons.play_arrow,
+        ),
+      ),
+    );
+  }
+}
